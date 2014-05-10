77b5a1a1e0bcad36834cd42983c5ef842daa9c8d
unknown <radhamme@gmail.com>
2014-05-10 14:59:30 -0500

    Assignment done

---
+++ plot2.R
@@ -0,0 +1,17 @@
+setwd("C:\\Coursera\\Data Specialization Track\\Repositories\\ExData_Plotting1")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl,"project1.zip")
+filename <- unzip("project1.zip")
+install.packages("sqldf")
+library(sqldf)
+mySql <- "Select * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+myData <- read.csv.sql(filename,mySql,sep=";")
+dateFr <- as.data.frame(paste(myData$Date,myData$Time), stringasFactors=FALSE)
+newdateFr <- strptime(dateFr[,1],format="%d/%m/%Y %H:%M:%S")
+finalData <- cbind(newdateFr, myData[3:9])
+png("plot2.png",width=480,height=480)
+plot(finalData[,1],finalData[,2], main="",xlab="",ylab="Global Active Power (kilowatts)",typ="l")
+dev.off()
+
+
+            
---
+++ plot3.R
@@ -0,0 +1,17 @@
+setwd("C:\\Coursera\\Data Specialization Track\\Repositories\\ExData_Plotting1")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl,"project1.zip")
+filename <- unzip("project1.zip")
+install.packages("sqldf")
+library(sqldf)
+mySql <- "Select * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+myData <- read.csv.sql(filename,mySql,sep=";")
+dateFr <- as.data.frame(paste(myData$Date,myData$Time), stringasFactors=FALSE)
+newdateFr <- strptime(dateFr[,1],format="%d/%m/%Y %H:%M:%S")
+finalData <- cbind(newdateFr, myData[3:9])
+png("plot3.png",width=480,height=480)
+plot(finalData[,1],finalData[,6],col="black",typ="l", main="",xlab="",ylab="Energy sub metering")
+lines(finalData[,1],finalData[,7],col="red",typ="l")
+lines(finalData[,1],finalData[,8],col="blue",typ="l")
+legend("topright", lty=1, col = c("black","blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
+dev.off()          
